24935ad393228f820ca94d3d53bc89206f5f816d
Kaheru Brian <bkaheru@student.refactory.academy>
2024-04-16 08:16:57 +0300

    created models

--- my-project/webapp/blog/models.py
+++ my-project/webapp/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class MyBlogs(models.Model):
+    titles = models.CharField(max_length=100)
+    content = models.TextField
+    date_posted = models.DateTimeField(auto_now_add=True)
+    taa = models.CharField(max_length=100)
+    image = models.TextField()
+    views = models.IntegerField(default=10)
